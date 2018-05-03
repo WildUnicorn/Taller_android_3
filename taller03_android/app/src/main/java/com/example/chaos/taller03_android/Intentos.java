eb760d3f19077713e0b6a383175dd6fd64e0bd0c
Unknown <germin_8x@hotmail.com>
2018-05-03 08:18:01 -0300

    Fragment Faltantes
    Fragment Faltantes listos!

---
+++ taller03_android/app/src/main/java/com/example/chaos/taller03_android/Intentos.java
@@ -0,0 +1,34 @@
+package com.example.chaos.taller03_android;
+
+/**
+ * Created by Darklook64 on 03-05-2018.
+ */
+
+public class Intentos {
+    private int aciertos;
+    private int fallos;
+
+    public Intentos(int aciertos, int fallos) {
+        this.aciertos = aciertos;
+        this.fallos = fallos;
+    }
+
+    public Intentos() {
+    }
+
+    public int getAciertos() {
+        return aciertos;
+    }
+
+    public void setAciertos(int aciertos) {
+        this.aciertos = aciertos;
+    }
+
+    public int getFallos() {
+        return fallos;
+    }
+
+    public void setFallos(int fallos) {
+        this.fallos = fallos;
+    }
+}
---
+++ taller03_android/app/src/main/java/com/example/chaos/taller03_android/Palabra.java
@@ -0,0 +1,24 @@
+package com.example.chaos.taller03_android;
+
+/**
+ * Created by Darklook64 on 03-05-2018.
+ */
+
+public class Palabra {
+    private String palabra;
+
+    public Palabra(String palabra) {
+        this.palabra = palabra;
+    }
+
+    public Palabra() {
+    }
+
+    public String getPalabra() {
+        return palabra;
+    }
+
+    public void setPalabra(String palabra) {
+        this.palabra = palabra;
+    }
+}
