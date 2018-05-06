db1ea66f87f1aab76c8978b2618f30626146d7a1
Chaos <jrcortest1@gmail.com>
2018-05-06 17:27:24 -0300

    Añadida validación a CambiarTiempo.

--- taller03_android/app/src/main/java/com/example/chaos/taller03_android/CambiarTiempoFragment.java
+++ taller03_android/app/src/main/java/com/example/chaos/taller03_android/CambiarTiempoFragment.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.chaos.taller03_android.model.Data;
@@ -18,6 +19,7 @@ import com.example.chaos.taller03_android.model.Data;
 
 public class CambiarTiempoFragment extends Fragment {
     private Data d;
+    private TextView lblTiempoActual;
     private EditText txtCambiarTiempo;
 
     @Override
@@ -28,7 +30,11 @@ public class CambiarTiempoFragment extends Fragment {
 
         d = new Data();
         txtCambiarTiempo = (EditText) view.findViewById(R.id.txtCambiarTiempo);
+        lblTiempoActual = (TextView) view.findViewById(R.id.lblTiempoActual);
         Button btnCambiarTiempo = (Button) view.findViewById(R.id.btnCambiarTiempo);
+
+        lblTiempoActual.setText("Tiempo actual: "+d.getTiempo()+" segundos.");
+
         btnCambiarTiempo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -39,33 +45,53 @@ public class CambiarTiempoFragment extends Fragment {
         return view;
     }
 
-
     public void cambiarTiempo(View v) {
         final int tiempo = Integer.parseInt(txtCambiarTiempo.getText().toString());
+        if (validarTiempo(tiempo) == true) {
 
-        AlertDialog.Builder confirmar = new AlertDialog.Builder(v.getContext());
-        confirmar.setTitle("Confirmar");
-        confirmar.setMessage("¿Desea cambiar el tiempo?");
-        confirmar.setCancelable(false);
-        confirmar.setPositiveButton("Sí", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                aceptar(tiempo);
-            }
-        });
+            AlertDialog.Builder confirmar = new AlertDialog.Builder(v.getContext());
+            confirmar.setTitle("Confirmar");
+            confirmar.setMessage("¿Desea cambiar el tiempo a "+tiempo+" segundos?");
+            confirmar.setCancelable(false);
+            confirmar.setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    aceptar(tiempo);
+                }
+            });
 
-        confirmar.setNegativeButton("No", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface confirmar, int i) {
-                confirmar.dismiss();
-            }
-        });
-        confirmar.show();
+            confirmar.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface confirmar, int i) {
+                    confirmar.dismiss();
+                }
+            });
+            confirmar.show();
+        }
+    }
+
+    public boolean validarTiempo(int tiempo) {
+        final boolean esValido;
+
+        if (tiempo > 100) {
+            Toast.makeText(getView().getContext(), "El tiempo máximo no puede ser superior a 100 segundos.", Toast.LENGTH_LONG).show();
+            esValido = false;
+
+        } else if (tiempo < 2) {
+            Toast.makeText(getView().getContext(), "El tiempo mínimo no puede ser inferior a 2 segundos.", Toast.LENGTH_LONG).show();
+            esValido = false;
+        } else {
+            esValido = true;
+        }
+
+        return esValido;
     }
 
     public void aceptar(int tiempo) {
         d.setTiempo(tiempo);
+        lblTiempoActual.setText("Tiempo actual: "+d.getTiempo()+" segundos.");
         Toast.makeText(getView().getContext(), "Nuevo tiempo asginado: " + tiempo + " segundos.", Toast.LENGTH_LONG).show();
+        txtCambiarTiempo.setText("");
     }
 
     ;
--- taller03_android/app/src/main/java/com/example/chaos/taller03_android/model/Data.java
+++ taller03_android/app/src/main/java/com/example/chaos/taller03_android/model/Data.java
@@ -3,6 +3,10 @@ package com.example.chaos.taller03_android.model;
 public class Data {
     private int tiempo;
 
+    public Data() {
+        this.tiempo = 5;
+    }
+
     public int getTiempo() {
         return tiempo;
     }
